d583f3acb8d3242093b62996123f8e2715854133
빅데이터과 김현규 <sisalbkim@gmail.com>
2025-03-26 15:41:28 +0900

    Add files via upload

--- C_study/3-8.c
+++ C_study/3-8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-	int  a,b ;
+	int  a, b ;
 	float  c, d;
 
 	a = 100;
@@ -10,7 +10,7 @@ void main()
 	c = 111.1f;
 	d = c; /*d에 c값을 대입함 */
 
-	printf("a, b의 값 ==> %d, %d \n",a ,b);
+	printf("a, b의 값 ==> %d , %d \n",a ,b);
 	printf("c, d의 값 ==> %5.1f , %5.1f \n",c ,d);
 }
 
