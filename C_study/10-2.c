586cbec735213853453142233e89edd36a01e5b4
빅데이터과 김현규 <sisalbkim@gmail.com>
2025-05-16 16:03:50 +0900

    Add files via upload

---
+++ C_study/10-2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int coffee_machine(int button){
+
+	printf("\n# 1. (자동으로) 뜨거운 물을 준비한다\n");
+	printf("# 2. (자동으로) 종이컵을 준비한다 \n");
+
+
+	switch (button){
+		case 1 : printf("# 3.(자동으로) 보통 커피를 탄다 \n"); break;
+		case 2 : printf("# 3.(자동으로) 설탕 커피를 탄다 \n"); break;
+		case 3 : printf("# 3.(자동으로) 블랙 커피를 탄다 \n"); break;
+		default : printf("# 3.(자동으로) 아무거나 탄다 \n"); break;
+	}
+	
+	printf("# 4. 물을 붓는다 \n");
+	printf("# 5. 스푼으로 저어서 녹인다 \n\n");
+
+	return 0;
+}
+
+
+void main()
+{
+	int coffee;
+	int ret;
+
+	printf("어떤 커피를 드릴까요? (1:보통 , 2:설탕 , 3:블랙) ");
+	scanf("%d", &coffee);
+
+	ret = coffee_machine(coffee);
+
+	printf("손님~ 커피 여기 있습니다. \n\n");
+}
+
