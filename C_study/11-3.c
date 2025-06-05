783439cca859786a50501bdb044e43b365d8c158
빅데이터과 김현규 <sisalbkim@gmail.com>
2025-06-05 17:12:14 +0900

    Add files via upload

---
+++ C_study/11-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	char ch;
+
+	printf("문자열을 입력하세요 : ");
+	ch = getchar();
+	putchar(ch);
+
+	ch = getchar();
+	putchar(ch);
+
+	ch = getchar();
+	putchar(ch);
+}
+
