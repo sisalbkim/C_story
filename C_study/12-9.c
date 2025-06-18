d2a796e04cbb0fb1410faa47a62b961f2a8a6bbe
빅데이터과 김현규 <sisalbkim@gmail.com>
2025-06-18 16:37:18 +0900

    Add files via upload

---
+++ C_study/12-9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void main()
+{
+
+	int *p;
+	int i , hap =0;
+	int cnt;
+
+	printf("입력할 개수는? : ");
+	scanf("%d",&cnt);
+
+	p = (int*) malloc(sizeof(int) * cnt);
+
+	for(i=0 ; i<cnt ; i++)
+	{
+		printf("%d 번째 숫자 : ",i+1);
+		scanf("%d", p+i);
+		
+		if (p[i]%2 == 0)
+			hap = hap + p[i];
+	}
+
+	printf("입력한 짝수합 ==> %d",hap);
+}
+
